8fadaac7914c40ee2444b350fbce83065fdf9bea
Jacat <jallen0506@gmail.com>
2015-05-29 11:54:52 -0400

    Main Class

---
+++ src/main/java/com/jacat/letsmod/LetsMod.java
@@ -0,0 +1,33 @@
+package com.jacat.letsmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "LetsMod", name = "LetsMod", version = "1.7.10-1.0")
+public class LetsMod
+{
+    @Mod.Instance("LetsMod")
+    public static LetsMod instance;
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
